0ca6b97688b3813cbac762078e48811083dfb49f
bilel04 <bilelchahed04@gmail.com>
2017-07-26 01:38:27 +0100

    test

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require ('express');
+const app= express();
+const mongoose = require('mongoose');
+const path = require ('path');
+const config= require('./config/database');
+const router = express.Router();
+const authentification = require('./routes/authentification')(router);
+
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+
+mongoose.Promise= global.Promise;
+mongoose.connect(config.uri, (err) => {
+	if(err) {
+		console.log('Echec de connexion au DB: ', err);
+	}
+	else {
+		console.log('Reussite de connexion au DB: ', config.db);
+	}
+});
+
+
+app.use(cors({
+	origin: 'http://localhost:4200'
+}));
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/admin/dist/'));
+app.use('/authentification', authentification);
+
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/admin/dist/index.html'));
+});
+
+app.listen(8080, () => {
+	console.log('Okey');
+});
